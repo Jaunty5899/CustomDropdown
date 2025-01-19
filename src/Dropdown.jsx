f9de282e875f89462342ef8a916764c5eb839756
Jaunty <jaunty5899@gmail.com>
2025-01-19 16:44:09 +0530

    menu complete

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -2,14 +2,24 @@ import "./Dropdown.css";
 import { useState } from "react";
 
 export default function Dropdown({ colors }) {
+  const [toggle, setToggle] = useState(false);
   const [choice, setChoice] = useState("Select Option");
-  console.log(colors);
   return (
     <div className="dropDownContainer">
-      <div className="dropDownButton">{choice}</div>
-      <div className="dropDownList">
+      <div className="dropDownButton" onClick={() => setToggle(true)}>
+        {choice}
+      </div>
+      <div className={`dropDownList ${toggle && "active"}`}>
         {colors.map((e) => (
-          <div className="dropDownOption">{e}</div>
+          <div
+            className={`dropDownOption`}
+            onClick={() => {
+              setChoice(e);
+              setToggle(false);
+            }}
+          >
+            {e}
+          </div>
         ))}
       </div>
     </div>
